73babf593c65d671268749ab712a82cf08e00e01
alandegenhart <alandegenhart@gmail.com>
2022-01-13 18:39:36 -0800

    Adding simple rnn test.

---
+++ examples/simple_rnn.py
@@ -0,0 +1,27 @@
+"""Simple recurrent neural network example."""
+
+# Imports
+import torch
+import torch.nn
+
+
+class SimpleRNN(torch.nn.Module):
+    """Simple recurrent neural network with output layer."""
+    pass
+
+
+def generate_data():
+    """Generate training data.
+
+    - Sinewave generator given frequency (see Sussillo and Barak 2013)
+    - Some other type of dynamical system?
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
